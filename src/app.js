8509a9910183d2639bf33ba380a7c11bf624f7c3
Edward Rodriguez <edwarderf@gmail.com>
2026-01-08 18:09:42 -0300

    Se comienza aplicacion de websockets

--- src/app.js
+++ src/app.js
@@ -3,8 +3,14 @@ import { engine } from "express-handlebars"
 import viewsRouter from "./routes/views.router.js";
 import productsRouter from "./routes/products.router.js";
 import cartsRouter from "./routes/carts.router.js";
+import { Server } from "socket.io";
+import http from "http";
 
 const app = express();
+const server = http.createServer(app);
+//websockets
+const io = new Server(server);
+
 // Habilitamos la carpeta public para archivos estaticos
 app.use( express.static("public") );
 
@@ -23,6 +29,21 @@ app.set("views", "./src/views");
 app.use("/", viewsRouter);
 app.use("/api/products", productsRouter);
 app.use("/api/carts", cartsRouter);
+
+//websockets desde el servidor
+io.on("connection", (socket)=> {
+  console.log("Nuevo usuario conectado");
+  //emitimos un evento desde el servidor al cliente
+  socket.emit("message history", messages);
+
+  //escuchamos un evento
+  socket.on("new message", (data)=> {
+    messages.push(data);
+
+    io.emit("broadcast new message", data);
+  });
+
+});
 
 app.listen(8080, ()=> {
   console.log("Servidor iniciado en el puerto 8080!");
