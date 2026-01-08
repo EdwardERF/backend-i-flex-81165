52ef834a9612469c24f2598acc7555689c686d12
Edward Rodriguez <edwarderf@gmail.com>
2026-01-08 17:38:40 -0300

    Se termina routeo de carts

--- src/app.js
+++ src/app.js
@@ -1,7 +1,5 @@
 import express from "express";
 import { engine } from "express-handlebars"
-import ProductManager from "./productManager.js";
-import CartManager from "./cartManager.js";
 import viewsRouter from "./routes/views.router.js";
 import productsRouter from "./routes/products.router.js";
 import cartsRouter from "./routes/carts.router.js";
@@ -25,42 +23,6 @@ app.set("views", "./src/views");
 app.use("/", viewsRouter);
 app.use("/api/products", productsRouter);
 app.use("/api/carts", cartsRouter);
-
-const cartManager = new CartManager("./src/carts.json");
-
-//endpoints de carts
-app.post( "/api/carts", async (req, res) => {
-  try {
-    const carts = await cartManager.addCart();
-    res.status(201).json({ message: "Carrito agregado", carts });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-app.get( "/api/carts/:cid", async (req, res) => {
-  try {
-    const cid = req.params.cid;
-
-    const cartProducts = await cartManager.getProductsByCartId(cid);
-    res.status(200).json({ message: "Se muestran lista de productos", cartProducts });  
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-app.post( "/api/carts/:cid/product/:pid", async (req, res) => {
-  try {
-
-    const cid = req.params.cid;
-    const pid = req.params.pid;
-    
-    const carts = await cartManager.addProductToCart(cid, pid);
-    res.status(201).json({ message: "Producto agregado a carrito", carts });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
 
 app.listen(8080, ()=> {
   console.log("Servidor iniciado en el puerto 8080!");
--- src/routes/carts.router.js
+++ src/routes/carts.router.js
@@ -4,4 +4,37 @@ import CartManager from "../cartManager.js";
 const cartsRouter = express.Router();
 const cartManager = new CartManager("./src/carts.json");
 
+cartsRouter.post("/", async (req, res) => {
+  try {
+    const carts = await cartManager.addCart();
+    res.status(201).json({ message: "Carrito agregado", carts });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+cartsRouter.get( "/:cid", async (req, res) => {
+  try {
+    const cid = req.params.cid;
+
+    const cartProducts = await cartManager.getProductsByCartId(cid);
+    res.status(200).json({ message: "Se muestran lista de productos", cartProducts });  
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+cartsRouter.post( "/:cid/product/:pid", async (req, res) => {
+  try {
+
+    const cid = req.params.cid;
+    const pid = req.params.pid;
+    
+    const carts = await cartManager.addProductToCart(cid, pid);
+    res.status(201).json({ message: "Producto agregado a carrito", carts });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 export default cartsRouter;
