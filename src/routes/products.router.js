d60ff5ec981d4d79926f2f69823f4d9103aa0877
Edward Rodriguez <edwarderf@gmail.com>
2026-01-15 19:28:28 -0300

    Se agrega metodo delete a la MongoDB para Products

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -6,33 +6,6 @@ import uploader from "../utils/uploader.js";
 const productsRouter = express.Router();
 const productManager = new ProductManager("./src/products.json");
 
-// productsRouter.post("/", uploader.single("file"), async(req, res)=> {
-//   try {
-//     //recibir data del formulario
-//     // TODO: Agregar parametros de mi producto
-    
-//     const title = req.body.title;
-//     const price = parseInt(req.body.price);
-//     const thumbnail = "/img/"+ req.file.filename;
-  
-//     await productManager.addProduct({ title, price, thumbnail });
-//     res.redirect("/dashboard");
-//   } catch (error) {
-//     console.log(error);
-//   }
-// })
-
-
-// Ruteo producto
-// productsRouter.get("/", async (req, res)=> {
-//   try {
-//     const products = await productManager.getProducts();
-//     res.status(200).json({ message: "Lista de productos actualizada", products: products });  
-//   } catch (error) {
-//     res.status(500).json({ message: error.message });
-//   }
-// });
-
 // Ruteo producto con Mongoose para get("/")
 productsRouter.get("/", async (req, res)=> {
   try {
@@ -44,27 +17,24 @@ productsRouter.get("/", async (req, res)=> {
   }
 });
 
+
+// Ruteo producto con Mongoose para delete("/:pid"
 productsRouter.delete("/:pid", async(req, res) => {
   try {
     const pid = req.params.pid;
 
-    const products = await productManager.deleteProductById(pid);
-    res.status(200).json({ message: "Producto eliminado", products });
+    // const products = await productManager.deleteProductById(pid);
+    const deletedProduct = await Product.findByIdAndDelete(pid);
+
+    // En caso de que el deletedProduct no exista, va a ser un caso en donde no encontró el Id, por lo que retornamos error.
+    if(!deletedProduct) return res.status(404).json({ status: "error", message: "Producto no encontrado" });
+
+    res.status(200).json({ status: "success", payload: deletedProduct });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ status: "error", message: "Error al eliminar producto" });
   }
 });
 
-// productsRouter.post("/", async (req, res) => {
-//   try {
-//     const newProduct = req.body;
-
-//     const products = await productManager.addProduct(newProduct);
-//     res.status(201).json({ message: "Producto agregado", products });
-//   } catch (error) {
-//     res.status(500).json({ message: error.message });
-//   }
-// });
 
 // Ruteo producto con Mongoose para post("/")
 productsRouter.post("/", async (req, res) => {
@@ -78,17 +48,6 @@ productsRouter.post("/", async (req, res) => {
   }
 });
 
-// productsRouter.put("/:pid", async (req, res) => {
-//   try {
-//     const pid = req.params.pid;
-//     const update = req.body;
-
-//     const updatedProducts = await productManager.setProductsById(pid, update);
-//     res.status(200).json({ message: "Producto actualizado", updatedProducts });
-//   } catch (error) {
-//     res.status(500).json({ message: error.message });
-//   }
-// })
 
 // Ruteo producto con Mongoose para put("/:pid"
 productsRouter.put("/:pid", async (req, res) => {
