62ba5a616dd567733ec46d48bd11def874e474b8
Edward Rodriguez <edwarderf@gmail.com>
2026-01-16 20:35:42 -0300

    Se agrega filtro por category

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -5,8 +5,16 @@ const viewsRouter = express.Router();
 
 viewsRouter.get("/", async (req, res, next)=> {
   try {
-    const { limit = 3, page = 1, order = "asc", orderBy = "title" } = req.query;
+    const { limit = 3, page = 1, order = "asc", orderBy = "title", category } = req.query;
 
+    // Agrego codigo para filtrado por categoria
+    const validCategories = ["Auriculares", "Teclados", "Mouse"];
+
+    const filter = {};
+    if (category && validCategories.includes(category)) {
+      filter.category = category;
+    }
+    
     // Agrego codigo para ordenamiento
     const validOrderBy = ["title", "price"];
     const validOrder = ["asc", "desc"];
@@ -17,14 +25,19 @@ viewsRouter.get("/", async (req, res, next)=> {
     const sortOptions = {};
     sortOptions[sortField] = sortOrder === "desc" ? -1 : 1;
     
-    const productsData = await Product.paginate({}, { limit, page, lean: true, sort: sortOptions });
+    const productsData = await Product.paginate(filter, { limit, page, lean: true, sort: sortOptions });
     const products = productsData.docs;
     delete productsData.docs;
 
     const links = [];
     
     for (let index = 1; index <= productsData.totalPages; index++) {
-      links.push({ text: index, link: `?limit=${limit}&page=${index}&order=${sortOrder}&orderBy=${sortField}` });
+      let link = `?limit=${limit}&page=${index}&order=${sortOrder}&orderBy=${sortField}`;
+      if (category && validCategories.includes(category)) {
+        link += `&category=${category}`;
+      }
+      
+      links.push({ text: index, link });
     };
 
     res.render("home", { products, links });
