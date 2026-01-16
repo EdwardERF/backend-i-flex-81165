7ff77d3119a98c12cda1c8cecd132ff60a050840
Edward Rodriguez <edwarderf@gmail.com>
2026-01-16 20:27:48 -0300

    Se agrega ordenamiento por title y price a Home.handlebars

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -5,16 +5,26 @@ const viewsRouter = express.Router();
 
 viewsRouter.get("/", async (req, res, next)=> {
   try {
-    const { limit = 3, page = 1 } = req.query;
+    const { limit = 3, page = 1, order = "asc", orderBy = "title" } = req.query;
 
-    const productsData = await Product.paginate({}, { limit, page, lean: true });
+    // Agrego codigo para ordenamiento
+    const validOrderBy = ["title", "price"];
+    const validOrder = ["asc", "desc"];
+    
+    const sortField = validOrderBy.includes(orderBy) ? orderBy : "title";
+    const sortOrder = validOrder.includes(order.toLowerCase()) ? order.toLowerCase() : "asc";
+    
+    const sortOptions = {};
+    sortOptions[sortField] = sortOrder === "desc" ? -1 : 1;
+    
+    const productsData = await Product.paginate({}, { limit, page, lean: true, sort: sortOptions });
     const products = productsData.docs;
     delete productsData.docs;
 
     const links = [];
     
     for (let index = 1; index <= productsData.totalPages; index++) {
-      links.push({ text: index, link: `?limit=${limit}&page=${index}` });
+      links.push({ text: index, link: `?limit=${limit}&page=${index}&order=${sortOrder}&orderBy=${sortField}` });
     };
 
     res.render("home", { products, links });
