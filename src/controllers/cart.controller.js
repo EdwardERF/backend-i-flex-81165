b3dff04969b2a2ea57e9650ee1b1e6467c29d7b0
Edward Rodriguez <edwarderf@gmail.com>
2026-01-16 19:42:27 -0300

    Se agrega codigo preventivo para carritos y productos incorrectos o productos ya existentes

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -1,5 +1,7 @@
 import Cart from "../models/cart.model.js";
+import Product from "../models/product.model.js";
 import { throwHttpError } from "../utils/httpError.js";
+import mongoose from "mongoose";
 
 export const createCart = async (req, res, next) => {
   try {
@@ -28,15 +30,38 @@ export const addProductToCart = async (req, res, next) => {
     const { cid, pid } = req.params;
     const { quantity } = req.body;
     
-    // Verificar que el producto exista
+    // Verificar que el carrito exista via Mongoose
+    if (!mongoose.Types.ObjectId.isValid(cid)) {
+      throwHttpError("Carrito no encontrado", 404);
+    }
+
+    // Verificar que el producto exista via Mongoose
+    if (!mongoose.Types.ObjectId.isValid(pid)) {
+      throwHttpError("Producto no encontrado", 404);
+    }
+    
+    // Verificar que el producto exista via logica interna
+    const productExists = await Product.findById(pid);
+    if (!productExists) throwHttpError("Producto no encontrado", 404);
 
     // Verificar que el carrito exista
+    const cart = await Cart.findById(cid)
+    if (!cart) throwHttpError("Carrito no encontrado", 404);
 
     // Verificar si el producto existe en el carrito
     // Si existe, incrementar la cantidad
     // Si no existe, agregarlo como nuevo
+    const existingProduct = cart.products.find( (cart) => cart.product.toString() === pid );
+
+    let updatedCart;
     
-    const updatedCart = await Cart.findByIdAndUpdate(cid, { $push: { products: { product: pid, quantity } } }, { new: true, runValidators: true })
+    if (existingProduct) {
+      // updatedCart = await Cart.findByIdAndUpdate(cid, { $inc: { "products.$.quantity": quantity } }, { new: true, runValidators: true })
+      updatedCart = await Cart.findOneAndUpdate({ _id: cid, "products.product": pid },{ $inc: { "products.$.quantity": quantity } },{ new: true, runValidators: true });  
+    } else {
+      updatedCart = await Cart.findByIdAndUpdate(cid, { $push: { products: { product: pid, quantity } } }, { new: true, runValidators: true })  
+    }
+
     res.status(201).json({ status: "success", payload: updatedCart });
   } catch (error) {
     next(error);
